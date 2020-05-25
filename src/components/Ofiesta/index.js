73023f8f93cdef03084409c18f3d66649f24e6e8
JulienMuller63 <julien.muller63@orange.fr>
2020-05-25 11:36:51 +0200

    searchbar mobile done

--- src/components/Ofiesta/index.js
+++ src/components/Ofiesta/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 
 // == Import
 import NavBar from 'src/components/NavBar';
+import Search from 'src/components/Search';
 
 // == Composant
 const Ofiesta = () => (
   <div className="ofiesta">
     <NavBar />
-    <div>search</div>
+    <Search />
     <div>cards</div>
     <div>themes</div>
     <div>footer</div>
---
+++ src/components/Search/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './search.scss';
+
+const Search = () => (
+  <div className="search">
+    <h1 className="search--title">OFIESTA</h1>
+    <h4 className="search--subtitle">Pour une fête sans prise de tête</h4>
+    <form className="search__form">
+      <input
+        className="search__form--input"
+        type="search"
+        placeholder="Quel prestataire?"
+      />
+      <input
+        className="search__form--input"
+        type="search"
+        placeholder="Département rechercher"
+      />
+      <button className="search__form--button" type="submit">Rechercher</button>
+    </form>
+  </div>
+);
+
+export default Search;
