ab63051df408bd213accded55d4822bfba78b468
JulienMuller63 <julien.muller63@orange.fr>
2020-05-25 08:35:16 +0200

    First commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <div>1</div>
+  </div>
+);
+
+// == Export
+export default App;
