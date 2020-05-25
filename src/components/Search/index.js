929c5d2b119aa2e5640fc74fde95b9ae6c5873d0
JulienMuller63 <julien.muller63@orange.fr>
2020-05-25 12:03:25 +0200

    searchbar done

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -4,21 +4,23 @@ import './search.scss';
 
 const Search = () => (
   <div className="search">
-    <h1 className="search--title">OFIESTA</h1>
-    <h4 className="search--subtitle">Pour une fête sans prise de tête</h4>
-    <form className="search__form">
-      <input
-        className="search__form--input"
-        type="search"
-        placeholder="Quel prestataire?"
-      />
-      <input
-        className="search__form--input"
-        type="search"
-        placeholder="Département rechercher"
-      />
-      <button className="search__form--button" type="submit">Rechercher</button>
-    </form>
+    <div className="search--content">
+      <h1 className="search--title">OFIESTA</h1>
+      <h4 className="search--subtitle">Pour une fête sans prise de tête</h4>
+      <form className="search__form">
+        <input
+          className="search__form--input"
+          type="search"
+          placeholder="Quel prestataire?"
+        />
+        <input
+          className="search__form--input"
+          type="search"
+          placeholder="Département rechercher"
+        />
+        <button className="search__form--button" type="submit">Rechercher</button>
+      </form>
+    </div>
   </div>
 );
 
