6d0386e6ae17add338bab5eac47c37731bae82b4
glemoin74 <glemoin@gmail.com>
2020-05-27 09:11:48 +0200

    Change on Connection Page

--- src/components/ConnectMembers/index.js
+++ src/components/ConnectMembers/index.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Field from 'src/components/Field';
-import { Icon } from 'semantic-ui-react';
 
 import './connectMembers.scss';
 
 const ConnectMembers = ({ changeField, email, password }) => {
-  const handleClickSignin = () => {
-    const signin = document.querySelector('.connectMembers-signin');
-    const signup = document.querySelector('.connectMembers-signup');
-    const arrowSignin = document.querySelector('#arrowSignin');
-    const arrowSignup = document.querySelector('#arrowSignup');
-    signin.classList.toggle('close');
-    signup.classList.toggle('close');
-    arrowSignin.classList.toggle('arrowClose');
-    arrowSignup.classList.toggle('arrowClose');
-  };
-  const handleClickSignup = () => {
-    const signin = document.querySelector('.connectMembers-signin');
-    const signup = document.querySelector('.connectMembers-signup');
-    const arrowSignin = document.querySelector('#arrowSignin');
-    const arrowSignup = document.querySelector('#arrowSignup');
-    signin.classList.toggle('close');
-    signup.classList.toggle('close');
-    arrowSignin.classList.toggle('arrowClose');
-    arrowSignup.classList.toggle('arrowClose');
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
   };
   return (
     <div className="connectMembers">
-      <h1 className="connectMembers-title" onClick={handleClickSignin}><span><Icon id="arrowSignin" name="caret down" className="" /></span>Se connecter à votre espace</h1>
-      <form className="connectMembers-signin">
+      <form className="connectMembers-signin" onSubmit={handleSubmit}>
+        <h1 className="connectMembers-title">Se connecter à votre espace</h1>
         <Field
           name="email"
           styles="field-input"
@@ -46,26 +28,6 @@ const ConnectMembers = ({ changeField, email, password }) => {
           value={password}
         />
         <button type="submit" className="adminConnect-submit">Se connecter</button>
-      </form>
-      <h1 className="connectMembers-title" onClick={handleClickSignup}><span><Icon id="arrowSignup" className="arrowClose" name="caret down" /></span>Créer votre espace membre</h1>
-      <form className="connectMembers-signup close">
-        <Field
-          name="gfdg"
-          styles="field-input"
-          placeholder="Votre adresse Email"
-  /*      onChange={changeField}
-          value={email} */
-        />
-        <Field
-          name="gfdgdf"
-          type="password"
-          styles="field-input"
-          placeholder="Votre mot de passe"
-  /*      onChange={changeField}
-          value={email} */
-        />
-        <button type="submit" className="adminConnect-submit">Se connecter</button>
-        <a href="#" className="adminConnect-forgot">Mot de passe oublié</a>
       </form>
     </div>
   );
