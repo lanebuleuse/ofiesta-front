62211127268d125d2a89359b916494d1e252d25e
JulienMuller63 <julien.muller63@orange.fr>
2020-05-27 14:26:57 +0200

    PIW

--- src/components/Ofiesta/index.js
+++ src/components/Ofiesta/index.js
@@ -4,20 +4,22 @@ import { Route, Switch } from 'react-router-dom';
 
 // == Import
 import NavBar from 'src/components/NavBar';
-import Page from 'src/components/Page';
 import Footer from 'src/components/Footer';
 import ConnectMembers from 'src/containers/ConnectMembers';
+import Search from 'src/components/Search';
+import Cards from 'src/components/Cards';
 
 // == Composant
 const Ofiesta = () => (
   <div className="ofiesta">
     <NavBar />
     <Switch>
+      <Route path="/">
+        <Search />
+        <Cards />
+      </Route>
       <Route path="/se-connecter">
         <ConnectMembers />
-      </Route>
-      <Route path="/">
-        <Page />
       </Route>
     </Switch>
     <Footer />
