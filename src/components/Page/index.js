b163ed9d2d7d844a0d3807928e3b74117c0ea94b
JulienMuller63 <julien.muller63@orange.fr>
2020-05-27 14:01:52 +0200

    Cards ok

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -4,8 +4,6 @@ import { Route } from 'react-router-dom';
 import Search from 'src/components/Search';
 import Cards from 'src/components/Cards';
 
-//import './page.scss';
-
 const Page = () => (
   <div className="page">
     <div className="page-content">
