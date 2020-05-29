e54ce391da946ae8aa62f1df6ef35425e414b275
glemoin74 <glemoin@gmail.com>
2020-05-29 09:34:14 +0200

    Minor Correction

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -5,15 +5,7 @@ import Home from 'src/components/Home';
 import { } from 'src/actions/user';
 
 const mapStateToProps = (state) => ({
-  title: state.service.title,
-  address: state.service.address,
-  postalCode: state.service.postalCode,
-  city: state.service.city,
-  department: state.service.department,
-  price: state.service.price,
-  note: state.service.note,
-  minGuest: state.service.minGuest,
-  maxGuest: state.service.maxGuest,
+
 });
 
 const mapDispatchToProps = (dispatch) => ({
