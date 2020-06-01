e2ea85fca046c1327b79457fa88f93dc2ecd6663
glemoin74 <glemoin@gmail.com>
2020-06-01 16:19:46 +0200

    First step of installation ok, work on localisation by adress

--- src/components/GoogleMaps/index.js
+++ src/components/GoogleMaps/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import GoogleMapReact from 'google-map-react';
+
 import './googlemaps.scss';
 
-const GoogleMaps = () => (
-  <div className="googlemaps">
-    test
-  </div>
-);
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const GoogleMaps = () => {
+  const defaultProps = {
+    center: {
+      lat: 48.8534,
+      lng: 2.3488,
+    },
+    zoom: 11,
+  };
+  return (
+    <div className="googlemaps">
+      <GoogleMapReact
+        /* bootstrapURLKeys={{ key: 'AIzaSyCuLVroL1_U2RRxad_rB99nnnUg3IRx7qw' }} */
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={48.8534}
+          lng={2.3488}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 export default GoogleMaps;
