497c23da281f20059d7fa40c2d08524089f82054
JulienMuller63 <julien.muller63@orange.fr>
2020-06-04 11:05:40 +0200

    add address creat prestataire

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -13,6 +13,7 @@ const Card = ({
   note,
   id,
   description,
+  department,
 }) => {
   const stars = [];
   for (let i = 0; i < 5; i += 1) {
@@ -33,8 +34,8 @@ const Card = ({
         alt=""
       />
       <div className="card__content">
-        <h3 className="card__content--title">{title}</h3>
-        <Badge key={ServiceList.id} variant="secondary">{ServiceList.name}</Badge>
+        <h3 className="card__content--title">{title} <span className="card__content--dep">({department})</span></h3>
+        <Badge className="card__content--badge" key={ServiceList.id} variant="secondary">{ServiceList.name}</Badge>
         <p className="card__content--text">
           {liteDescription}
         </p>
@@ -67,6 +68,7 @@ Card.propTypes = {
   note: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
   description: PropTypes.string.isRequired,
+  department: PropTypes.string.isRequired,
 };
 
 export default Card;
